a5f7a7a579db59e3bdd9a009d4cf956d1b3dd823
ankitjha <ankitjha20022020@gmail.com>
2022-05-25 00:21:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var rnum1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice"+rnum1+".png"; 
+
+var randomImageSource = "images/"+randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var rnum2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice"+rnum2+".png";
+var randomImageSource2 = "images/"+randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(rnum1 > rnum2){
+    document.querySelector("h1").innerHTML = "Player 1 WINS!";
+}
+else if(rnum1 < rnum2){
+    document.querySelector("h1").innerHTML = "Player 2 WINS!";
+}else{
+    document.querySelector("h1").innerHTML = "Its a DRAW!!";
+}
